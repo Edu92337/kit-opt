dc7a19ee3d89639d1d2ffda02dc8f459460db2d8
Eduardo Augusto <eduz92337@gmail.com>
2026-08-13 15:09:25 -0300

    Minimum spanning tree com DSU

--- utils/dsu.cpp
+++ utils/dsu.cpp
@@ -1,8 +1,10 @@
 #include<vector>
 #include<iostream>
-
+#include<algorithm>
+#include"Data.h"
 
 typedef struct Dsu {
+    int nos;
     std::vector<int> pais;
     //Heurística de tamanho
     std::vector<int> size;
@@ -10,11 +12,24 @@ typedef struct Dsu {
     void make_set(int v);
     int find_set(int v);
     void union_set(int a, int b);
+
+    //Minimum spanning tree
+    double** mst;
+    void gera_mst(Data* d);
+
+
 } Dsu;
 
 Dsu::Dsu(int n){
+    nos = n;
     pais.resize(n);
     size.resize(n);
+    // Aloca a Árvore como matriz no mesmo formato de Data
+    mst = new double* [n];
+    for(int i = 0;i<n;i++){
+        mst[i] = new double[n];
+        for(int j = 0;j < n;j++) mst[i][j] = 0;
+    }
 }
 
 void Dsu::make_set(int v){
@@ -35,5 +50,35 @@ void Dsu::union_set(int a, int b){
         if(size[a] < size[b]) std::swap(a,b);
         pais[b] = a;
         size[a] += size[b];
+    }
+}
+
+//Gera a mst com Kruskal sem ordenar
+void Dsu::gera_mst(Data* d){
+    for(int i = 0;i<nos;i++) make_set(i);
+    int qnt = 0;
+    
+    while(qnt<nos-1){
+
+        double menor = __DBL_MAX__;
+        int u = -1;
+        int v = -1;
+
+        for(int i = 0;i<nos;i++){
+            for(int j = 0;j<nos;j++){
+                if(find_set(i) == find_set(j))continue;
+                double distancia = d->getDistance(i,j);
+
+                if(distancia < menor){
+                    menor = distancia;
+                    u = i;
+                    v = j;
+                }
+            }
+        }
+        union_set(u,v);
+        mst[u][v] = menor;
+        mst[v][u] = menor;
+        qnt++;
     }
 }

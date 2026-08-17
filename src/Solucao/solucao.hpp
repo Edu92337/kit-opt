0dcb6ac2f04556ac48efcd416d02d50469a6cb7c
Eduardo Augusto <eduz92337@gmail.com>
2026-08-17 10:46:41 -0300

    comentários

--- src/Solucao/solucao.hpp
+++ src/Solucao/solucao.hpp
@@ -6,9 +6,9 @@ typedef struct Solucao Solucao;
 
 struct Solucao {
     Solucao(Data *data_original);
-    Data* data;
+    Data* data; // ponteiro para o objeto Data que vai ser passado para todas as soluções
     std::vector<int>sequencia;
-    double valor_obj;
+    double valor_obj; // custo da solução 
     void exibe_solucao();
     void add_no(double x);
     void calcula_valor_obj();
